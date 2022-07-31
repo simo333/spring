01b1654bf83663cc06a13ecba8d0bb22c5e0297d
simo333 <szymon.333@wp.pl>
2022-07-31 12:25:26 +0200

    Added random with max param Added getting header's user-agent param

--- jee-spring-mvc/src/main/java/pl/coderslab/controller/RandomController.java
+++ jee-spring-mvc/src/main/java/pl/coderslab/controller/RandomController.java
@@ -2,6 +2,8 @@ package pl.coderslab.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Random;
@@ -14,5 +16,19 @@ public class RandomController {
     public String showRandom() {
         Random random = new Random();
         return "Wylosowano liczbę: " + random.nextInt(1, 101);
+    }
+
+    @GetMapping("/random/{max}")
+    @ResponseBody
+    public String showRandomWithMax(@PathVariable int max) {
+        Random r = new Random();
+        int rand = r.nextInt(max) + 1;
+        return String.format("Użytkownik podał wartość %d. Wylosowano liczbę: %d.", max, rand);
+    }
+
+    @GetMapping("/header")
+    @ResponseBody
+    public String header(@RequestHeader("user-agent") String userAgent) {
+        return userAgent;
     }
 }
