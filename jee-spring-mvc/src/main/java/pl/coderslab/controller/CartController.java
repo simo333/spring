d9ae5e602f4db91b25c6246530edb80f402dc75c
simo333 <szymon.333@wp.pl>
2022-07-31 17:13:14 +0200

    Added new mapping

---
+++ jee-spring-mvc/src/main/java/pl/coderslab/controller/CartController.java
@@ -0,0 +1,35 @@
+package pl.coderslab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.coderslab.model.Cart;
+import pl.coderslab.model.CartItem;
+import pl.coderslab.model.Product;
+
+import java.util.Random;
+
+@Controller
+public class CartController {
+
+    private final Cart cart;
+
+    public CartController(Cart cart) {
+        this.cart = cart;
+    }
+
+    @RequestMapping("/addtocart")
+    @ResponseBody
+    public String addToCart() {
+        Random rand = new Random();
+        cart.addToCart(new CartItem(1, new Product("prod" + rand.nextInt(10), rand.nextDouble())));
+        return "addtocart";
+    }
+
+    @RequestMapping("/cart")
+    @ResponseBody
+    public String showCart() {
+        return cart.getCartItems().toString();
+    }
+
+}
