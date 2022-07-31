66114f72ae14e740cad0a098729cba0be9dcdbab
simo333 <szymon.333@wp.pl>
2022-07-31 13:11:47 +0200

    Added JSP with form and action to operate with this form. Added class level RequestMapping for HelloController.

--- jee-spring-mvc/src/main/java/pl/coderslab/controller/HelloController.java
+++ jee-spring-mvc/src/main/java/pl/coderslab/controller/HelloController.java
@@ -1,15 +1,32 @@
 package pl.coderslab.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/first")
 public class HelloController {
 
     @GetMapping("/hello")
     @ResponseBody
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("/hello/{firstName}/{lastName}")
+    @ResponseBody
+    public String hello(@PathVariable String firstName, @PathVariable String lastName) {
+        return String.format("Witaj %s %s", firstName, lastName);
+    }
+
+    @GetMapping("/form")
+    public String hello() {
+        return "index.jsp";
+    }
+
+    @PostMapping("/form")
+    @ResponseBody
+    public String processForm(@RequestParam String paramName, @RequestParam String paramDate) {
+        return paramName + paramDate;
     }
 }
---
+++ jee-spring-mvc/src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form>
+    <label>
+        Param name:
+        <input type="text" name="paramName" />
+    </label>
+    <label>
+        Param date:
+        <input type="text" name="paramDate" />
+    </label>
+    <button type="submit">Wyślij</button>
+</form>
+</body>
+</html>
