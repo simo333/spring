6f8fabff4904fc69e20076ebd04e6031a5c3cb0d
simo333 <szymon.333@wp.pl>
2022-08-19 18:09:21 +0200

    Mock Exam tasks - 3

---
+++ Mock Exam/src/main/java/pl/simo333/HeroComponent.java
@@ -0,0 +1,26 @@
+package pl.simo333;
+
+import pl.simo333.model.Hero;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class HeroComponent {
+    private List<Hero> heroList = new ArrayList<>();
+
+    public void addHero(Hero hero) {
+        heroList.add(hero);
+    }
+
+    public List<Hero> getHeroList() {
+        return heroList;
+    }
+
+    public Optional<Hero> findHeroByPhoneNumber(String phoneNumber) {
+        return heroList.stream()
+                .filter(hero -> phoneNumber.equals(hero.getPhone()))
+                .findFirst();
+    }
+
+}
