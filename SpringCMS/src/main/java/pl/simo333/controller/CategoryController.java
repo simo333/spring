0c27a674c382d72b82c1fd058cf9803bb1297b61
simo333 <szymon.333@wp.pl>
2022-09-03 09:26:42 +0200

    Spring and Hibernate part 2 - task 3 (part 2)

--- SpringCMS/src/main/java/pl/simo333/controller/CategoryController.java
+++ SpringCMS/src/main/java/pl/simo333/controller/CategoryController.java
@@ -3,6 +3,7 @@ package pl.simo333.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.simo333.domain.Category;
 import pl.simo333.repository.CategoryDao;
@@ -17,8 +18,13 @@ public class CategoryController {
     }
 
     @GetMapping("/add")
-    public String addCategory(Category category, Model model) {
+    public String addCategory(Model model) {
         model.addAttribute("category", new Category());
+        return "category-add";
+    }
+    @PostMapping("/add")
+    public String addCategory(Category category) {
+        categoryDao.save(category);
         return "category-add";
     }
 }
--- SpringCMS/src/main/webapp/WEB-INF/views/category-add.jsp
+++ SpringCMS/src/main/webapp/WEB-INF/views/category-add.jsp
@@ -1,9 +1,16 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>Add a new Category</title>
 </head>
 <body>
-
+<form:form method="post" modelAttribute="category">
+    <legend>New category:</legend>
+    <form:hidden path="id" value="${category.id}"/>
+    Name: <form:input path="name" value="${category.name}"/><br>
+    Description: <form:input path="description" value="${category.description}"/><br>
+    <button type="submit">Save</button>
+</form:form>
 </body>
 </html>
