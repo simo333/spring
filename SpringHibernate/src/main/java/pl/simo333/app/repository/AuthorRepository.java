cd41ccaa145176fe88037b0c9f4cea7d5501ac5b
simo333 <szymon.333@wp.pl>
2022-09-04 09:57:48 +0200

    Detailed queries - task 3

--- SpringHibernate/src/main/java/pl/simo333/app/repository/AuthorRepository.java
+++ SpringHibernate/src/main/java/pl/simo333/app/repository/AuthorRepository.java
@@ -1,6 +1,7 @@
 package pl.simo333.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.simo333.app.domain.Author;
 
 import java.util.List;
@@ -13,5 +14,11 @@ public interface AuthorRepository extends JpaRepository<Author, Long> {
     Optional<Author> findByPesel(String pesel);
 
     List<Author> findAllByLastName(String lastName);
+
+    @Query("select a from Author a where a.email like ?1%")
+    List<Author> queryByEmailLike(String email);
+
+    @Query("select a from Author a where a.pesel like ?1%")
+    List<Author> queryByPeselLike(String pesel);
 
 }
