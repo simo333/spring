0a0c89d8860f12cbc511ab853a4ad4bcf123f7be
simo333 <szymon.333@wp.pl>
2022-09-08 21:05:49 +0200

    Spring and Hibernate part 2 - task 3 (part 3)

--- SpringCMS/src/main/java/pl/simo333/controller/CategoryController.java
+++ SpringCMS/src/main/java/pl/simo333/controller/CategoryController.java
@@ -3,6 +3,7 @@ package pl.simo333.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.simo333.domain.Category;
@@ -17,14 +18,40 @@ public class CategoryController {
         this.categoryDao = categoryDao;
     }
 
+    @GetMapping
+    public String showCategories(Model model) {
+        model.addAttribute("categories", categoryDao.findAll());
+        return "category/all";
+    }
+
     @GetMapping("/add")
     public String addCategory(Model model) {
         model.addAttribute("category", new Category());
-        return "category-add";
+        return "category/add";
     }
+
     @PostMapping("/add")
     public String addCategory(Category category) {
         categoryDao.save(category);
-        return "category-add";
+        return "category/add";
     }
+
+    @GetMapping("/edit/{id}")
+    public String editCategory(Model model, @PathVariable Long id) {
+        model.addAttribute("category", categoryDao.findById(id));
+        return "category/add";
+    }
+
+    @PostMapping("/edit")
+    public String editCategory(Category category) {
+        categoryDao.update(category);
+        return "category/add";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteCategory(@PathVariable Long id) {
+        categoryDao.delete(categoryDao.findById(id));
+        return "category/add";
+    }
+
 }
